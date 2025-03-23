0d0d34c62c34aa1c3e0162df580c1c909ff44919
lidorbashari <lidor1965@gmail.com>
2025-03-23 12:48:59 +0200

    change author name

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <p>
         Made by{' '}
         <a href='https://github.com/ayushiee/nextflix' target='_blank' rel='noreferrer'>
-          <b>Ayushi Gupta and lidor</b>
+          <b>Ayushi Gupta and lidor bashari</b>
         </a>
       </p>
     </div>
