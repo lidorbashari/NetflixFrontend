e4a0cfd58ef75f4e67815b38e49c6b949ce81a5e
lidorbashari <lidor1965@gmail.com>
2025-03-28 09:45:03 +0300

    change name!

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <p>
         Made by{' '}
         <a href='https://github.com/ayushiee/nextflix' target='_blank' rel='noreferrer'>
-          <b>Ayushi Gupta!!!</b>
+          <b>Ayushi Gupta!!!!!</b>
         </a>
       </p>
     </div>
